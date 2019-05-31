50ccd6c549643896b523a62e4aad19e3aa1ad656
Alexandros Kosiaris <akosiaris@wikimedia.org>
2019-05-31 18:23:11 +0300

    Fix typo in initialize_service.sh
    Add a forgotten "i" character
    
    Change-Id: I1bac68f099dfeb6523a1d8deb6d6a8c8d8a9dda6

--- initialize_service.sh
+++ initialize_service.sh
@@ -7,6 +7,6 @@ SERVICE=$1
 
 for i in eqiad codfw staging
 do
-	sudo KUBECONFIG=/etc/kubernetes/admin-${i}.config ./initalize_namespace.sh $SERVICE $SERVICE
+	sudo KUBECONFIG=/etc/kubernetes/admin-${i}.config ./initialize_namespace.sh $SERVICE $SERVICE
 	sudo KUBECONFIG=/etc/kubernetes/admin-${i}.config kubectl -n $SERVICE patch deploy tiller-deploy --patch "$(cat tiller/${i}.yaml)"
 done
