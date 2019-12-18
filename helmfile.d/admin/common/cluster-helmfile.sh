1d262a639b0143b921bda764ac938c04d0b943b4
Alexandros Kosiaris <akosiaris@wikimedia.org>
2019-12-18 17:42:26 +0200

    Deduplicate cluster-helmfile.sh
    Why:
    DRY should be a principle
    
    What:
    Move it at the top of the repo and add symlinks
    
    Change-Id: Ic00e4db31800f079d9a38daf26801a5d36fe9daa

---
+++ helmfile.d/admin/common/cluster-helmfile.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+pushd podsecuritypolicies
+    helmfile "$@"
+popd
+pushd rbac
+    helmfile "$@"
+popd
+pushd coredns
+    helmfile "$@"
+popd
+pushd calico
+    helmfile "$@"
+popd
+for NS in $(ls namespace/values/*.yaml); do helmfile -e $(basename -s .yaml $NS) "$@" ;done
--- helmfile.d/admin/eqiad/cluster-helmfile.sh
+++ helmfile.d/admin/eqiad/cluster-helmfile.sh
@@ -1,14 +1,1 @@
-#!/bin/bash
-pushd podsecuritypolicies
-    helmfile "$@"
-popd
-pushd rbac
-    helmfile "$@"
-popd
-pushd coredns
-    helmfile "$@"
-popd
-pushd calico
-    helmfile "$@"
-popd
-for NS in $(ls values/*.yaml); do helmfile -e $(basename -s .yaml $NS) "$@" ;done
+../common/cluster-helmfile.sh
