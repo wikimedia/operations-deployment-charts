37f33bad9c38d4a584c68859d1fdd1f479f92640
Balthazar Rouberol <brouberol@wikimedia.org>
2024-02-26 17:37:49 +0100

    superset: assign Superset roles from LDAP groups
    We define a mapping of LDAP group (cn) to Superset roles.
    As we set auth_roles_sync_at_login=True, this means that a Superset
    user roles will get updates at each refresh.
    
    We also make sure to update the Superset User model with the details
    provided to us by the OIDC provider. For example, if an existing user
    had an email of brouberol@email.notfound and the OIDC provider returns
    `{..., 'email': 'brouberol@wikimedia.org'}, we make sure to automatically
    update the Superset User record with these details.
    
    Bug: T297120
    Bug: T353794
    Change-Id: I6d0c6b2ea3e4709caf691382ff4e9bdd1ece1de1

--- charts/superset/files/custom_sso_security_manager.py
+++ charts/superset/files/custom_sso_security_manager.py
@@ -1,4 +1,5 @@
 import logging
+from typing import Dict
 
 from superset.security import SupersetSecurityManager
 
@@ -6,13 +7,31 @@ logger = logging.getLogger(__name__)
 
 
 class CustomSsoSecurityManager(SupersetSecurityManager):
-    def oauth_user_info(self, provider, response=None):
+    def auth_user_oauth(self, userinfo: Dict):
+        """Update the user details if need be.
+
+        This method overrides the auth_user_oauth method detailed at
+        https://flask-appbuilder.readthedocs.io/en/latest/_modules/flask_appbuilder/security/manager.html#BaseSecurityManager.auth_user_oauth
+        and make sure to update the user with the details provided by the
+        OIDC provider (name, username, email).
+
+        """
+        user = super().auth_user_oauth(userinfo)
+        if user and self.auth_user_registration:
+            user.first_name = userinfo["name"]
+            user.username = userinfo["username"]
+            user.email = userinfo["email"]
+            self.update_user_auth_stat(user)
+        return user
+
+    def oauth_user_info(self, provider: str, response=None) -> Dict:
         if provider == "CAS":
             me = self.appbuilder.sm.oauth_remotes[provider].userinfo()
-            logger.info(f"Login with user info {me}")
-            return {
+            userinfo = {
                 "id": me["id"],
                 "username": me.get("preferred_username", me["id"]),
                 "name": me.get("name", me["id"]),
                 "email": me.get("email", f"{me['id']}@email.notfound"),
+                "role_keys": me.get("memberOf", []),
             }
+            return userinfo
