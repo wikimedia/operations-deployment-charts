9e4555f7030beac36d9bf4a38a2a2d2822280fa2
Balthazar Rouberol <brouberol@wikimedia.org>
2026-09-08 14:54:50 +0200

    airflow: ensure emails are logged but not sent in devenvs
    This will ensure we don't pollute opsweek emails for development
    failures
    
    Signed-off-by: Balthazar Rouberol <brouberol@wikimedia.org>
    Bug: T416596
    Change-Id: I9a3a4634eea8b1208b9822831986f0d4935a7666

--- charts/airflow/files/webserver_config/custom_funcs.py
+++ charts/airflow/files/webserver_config/custom_funcs.py
@@ -1,5 +1,17 @@
 import os
+import logging
+
+from airflow.utils.email import send_email_smtp
+
+log = logging.getLogger(__name__)
+
 
 def get_scheduler_service_name():
     """Function used by the webserver to return the scheduler internal service name"""
-    return os.environ['AIRFLOW_SCHEDULER_HOSTNAME']
+    return os.environ["AIRFLOW_SCHEDULER_HOSTNAME"]
+
+
+def send_email_smtp_dry_run(*args, **kwargs):
+    kwargs["dryrun"] = True
+    log.info(f"[DRY RUN] send_email_smtp, kwargs={kwargs}")
+    return send_email_smtp(*args, **kwargs)
